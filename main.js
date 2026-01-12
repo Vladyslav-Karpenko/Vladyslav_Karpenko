52790c2ad60d1105b904b3d932b38d9384bb0d99
Vladyslav Karpenko <vladyslav.karpenko@ukr.net>
2026-01-13 00:42:50 +0100

    fixed bugs with doctype and script out of body

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
 
-const header = document.querySelector('#header');
+// !Welcome screen
+const enterBtn = document.getElementById("enterBtn");
+const intro = document.getElementById("intro");
+const bgMusic = document.getElementById("bgMusic");
+const content = document.getElementById("content");
+if (localStorage.getItem("entered")) {
+    intro.style.display = "none";
+    content.classList.remove("hidden");
+    document.body.style.overflow = "auto";
+}
+enterBtn.addEventListener("click", () => {
+    intro.style.opacity = "0";
+    setTimeout(() => {
+        intro.style.display = "none";
+        content.classList.remove("hidden");
+        document.body.style.overflow = "auto";
+        bgMusic.currentTime = 0;
+        bgMusic.volume = 0.6;
+        bgMusic.play().catch((err) => console.log("Music play blocked:", err));
+        localStorage.setItem("entered", "true");
+    }, 1000);
+});
 
+const header = document.querySelector('#header');
 if (header) {
     fetch('header.html')
         .then(response => response.text())
         .then(html => {
             header.innerHTML = html;
-            // !Welcome screen
-            const enterBtn = document.getElementById("enterBtn");
-            const intro = document.getElementById("intro");
-            const bgMusic = document.getElementById("bgMusic");
-            const content = document.getElementById("content");
-            if (localStorage.getItem("entered")) {
-                intro.style.display = "none";
-                content.classList.remove("hidden");
-                document.body.style.overflow = "auto";
-            }
-            enterBtn.addEventListener("click", () => {
-                intro.style.opacity = "0";
-                setTimeout(() => {
-                    intro.style.display = "none";
-                    content.classList.remove("hidden");
-                    document.body.style.overflow = "auto";
-                    bgMusic.currentTime = 0;
-                    bgMusic.volume = 0.6;
-                    bgMusic.play().catch((err) => console.log("Music play blocked:", err));
-                    localStorage.setItem("entered", "true");
-                }, 1000);
-            });
             // ! Hamburger
             const hamburger = document.getElementById("hamburger");
             const navList = document.getElementById("navList");
